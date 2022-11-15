979fc1b9706c6e07b169a95fe1ef125432704178
pasqualedem <pasquale199876@hotmail.it>
2022-11-15 20:06:06 +0100

    Commit dal server

--- detect.py
+++ detect.py
@@ -1,3 +1,19 @@
+import numpy as np
+
 from detector import CropRowDetector
 
 
+def get_line_boxes(theta, rho, is_deg, img_width, img_height):
+    if is_deg:
+        theta = np.deg2rad(theta)
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = (a * rho) + img_width / 2
+    y0 = (b * rho) + img_height / 2
+    x1 = int(x0 + img_width * (-b))
+    y1 = int(y0 + img_height * (a))
+    x2 = int(x0 - img_width * (-b))
+    y2 = int(y0 - img_height * (a))
+    return (x1, y1), (x2, y2)
+
+def get_square_from_lines(t)
