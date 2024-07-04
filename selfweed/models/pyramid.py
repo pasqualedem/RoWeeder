ffc081ad79a695b5afbe121f1c270d3b57b33687
pasqualedem <pasquale199876@hotmail.it>
2024-07-04 16:34:29 +0200

    Implements PyramidFormer

--- selfweed/models/pyramid.py
+++ selfweed/models/pyramid.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from selfweed.models.utils import RowWeederModelOutput
+
 class PyramidFormer(nn.Module):
     def __init__(
         self,
@@ -14,21 +16,30 @@ class PyramidFormer(nn.Module):
         self.embedding_dims = embedding_dims
         
         self.pyramid_fusers = nn.ModuleList([
-            PyramidFuser(dim_shallow, dim_deep) for dim_shallow, dim_deep in zip(embedding_dims, embedding_dims[1:])
+            PyramidFuser(dim_shallow, dim_deep) for dim_shallow, dim_deep in zip(embedding_dims[::-1], embedding_dims[::-1][1:])
         ])
         self.classifier = nn.Conv2d(embedding_dims[0], num_classes, kernel_size=1)
         
-    def forward(self, x):
-        hidden_states = self.encoder(x)
+    def forward(self, image):
+        B, _, H, W = image.shape
+        hidden_states = self.encoder(image, output_hidden_states=True).hidden_states
         x = hidden_states[-1]
-        for fuser in self.pyramid_fusers:
-            x = fuser(hidden_states.pop(-2), x)
-        x = self.classifier(x)
-        return x
+        for i, fuser in enumerate(self.pyramid_fusers):
+            x = fuser(hidden_states[-i-2], x)
+        logits = self.classifier(x)
+        logits = F.interpolate(logits, size=(H, W), mode="bilinear")
+        return RowWeederModelOutput(logits=logits, scores=None)
+    
+    def get_learnable_params(self, train_params):
+        if train_params.get("freeze_encoder", False):
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+        params = filter(lambda p: p.requires_grad, self.parameters())
+        return [{"params": params}]
         
         
 class PyramidFuser(nn.Module):
-    def __init__(self, dim_shallow, dim_deep, activation="GELU"):
+    def __init__(self, dim_deep, dim_shallow, activation="GELU"):
         super().__init__()
         self.fuse_conv = nn.Conv2d(dim_shallow + dim_deep, dim_shallow, kernel_size=1)
         self.spatial_fuse = nn.Conv2d(dim_shallow, dim_shallow, kernel_size=3, padding=1)
